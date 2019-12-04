deee4cb20438236218740f6a6aa816199bdf56a1
Brian Mitchell <brian@nowsta.com>
2019-12-03 19:31:30 -0500

    Add answers test
    This will ensure that old answers which share code don't
    break as new things are added.

---
+++ test/answers_test.exs
@@ -0,0 +1,20 @@
+defmodule AnswersTest do
+  use ExUnit.Case
+
+  test "star 1" do
+    assert Advent.star1 == 3412207
+  end
+
+  test "star 2" do
+    assert Advent.star2 == 5115436
+  end
+
+  test "star 3" do
+    assert Advent.star3 == 3058646
+  end
+
+  test "star 4" do
+    assert Advent.star4 == 8976
+  end
+
+end
